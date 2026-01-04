b26a56ef0ea30f66e61df2be02b0ab332115a168
ferney1972 <produccionsanchezparra@gmail.com>
2026-01-04 17:28:26 +0100

    Fix CartContext addItem

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,19 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "misquince.es",
+      },
+      {
+        protocol: "https",
+        hostname: "videos.pexels.com",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
+
