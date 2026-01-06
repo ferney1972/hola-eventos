552bf1ac6d54d632a78e2816aedb154cb15b3a8c
ferney1972 <produccionsanchezparra@gmail.com>
2026-01-06 12:42:47 +0100

    Actualizar rutas de imágenes y envío de email

--- lib/products.ts
+++ lib/products.ts
@@ -33,7 +33,7 @@ export const products: Product[] = [
     id: "sillas-plegables",
     name: "Silla plegable",
     image: {
-      src: "https://misquince.es/fotos/silla-plegable.jpg",
+      src: "https://misquince.es/fotos/sillas negras.PNG",
     },
     price: 1.5,
     description:
@@ -43,7 +43,7 @@ export const products: Product[] = [
     id: "tarimas-escenario",
     name: "Tarimas para escenario",
     image: {
-      src: "https://misquince.es/fotos/tarimas-escenario.jpg",
+      src: "https://misquince.es/fotos/Alquiler de escenario.png",
     },
     price: 40,
     description:
