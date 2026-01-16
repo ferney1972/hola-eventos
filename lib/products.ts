e9e4b7790bbd99067cda27489a45111065b8ebae
ferney1972 <produccionsanchezparra@gmail.com>
2026-01-16 16:06:13 +0100

    Corregir products.ts

--- lib/products.ts
+++ lib/products.ts
@@ -54,7 +54,7 @@ export const products: Product[] = [
     id: "carpa-3x3m",
     name: "Carpa de 3x3m",
     image: {
-      src: "https://misquince.es/fotos/alquilercarpade3x3m.jpg",
+      src: "https://misquince.es/fotos/alquiler carpa de 3x3m.jpg",
     },
     price: 40,
     description:
