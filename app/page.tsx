adf554c6206e172d3f97c9b6b9e6f256b144bba6
ferney1972 <produccionsanchezparra@gmail.com>
2026-03-26 16:41:11 +0100

    cambio page

--- app/page.tsx
+++ app/page.tsx
@@ -33,7 +33,7 @@ export default function Home() {
     "Expositor",
     "Sonido",
     "Ventilador-nebulizador",
-    "Cantenaria"
+    "Cantenaria-dorada"
   ];
 
   const featuredProducts = products
